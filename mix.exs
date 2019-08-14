dafee4e964d5cf00f4c8a41fc0aa84c77499d783
Kenton <smartepsh@gmail.com>
2019-08-15 01:55:24 +0800

    chore: add some basic dependencies

--- mix.exs
+++ mix.exs
@@ -33,7 +33,10 @@ defmodule Decanter.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: :dev, runtime: false},
+      {:credo, "~> 1.0.0", only: :dev, runtime: false},
+      {:gnuplot, "~> 1.19"}
     ]
   end
 end
